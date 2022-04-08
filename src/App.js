8c2d486a28b29b622ef7e33e3c6fd529145ad697
Max Sealey <maxsealey@users.noreply.github.com>
2022-04-07 19:21:38 -0700

    reset

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render(){
+    return null
+  }
 }
 
 export default App;
