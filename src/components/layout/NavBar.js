383fa68b4d0e53d01b50e96e8998f2b9b91a2e56
Max Sealey <maxsealey@users.noreply.github.com>
2022-05-09 11:48:05 -0700

    navbar styling, added text and link

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react'
+import '../../App.css'
 
 export default class NavBar extends Component {
   render() {
     return (
-      <div></div>
+        <div>
+            <nav className='navbar navbar-expand-md navbar-dark bg-dark fixed-top' 
+            style={{ height:'50px', display:'flex', justifyContent:'space-around' }}>
+            <a href="#" style={{ color:'white', textDecoration:'none', margin:'15px 0px 0px 15px' }}>
+            <h5>Pokedex - Gotta Catch 'Em All!</h5></a></nav>
+        </div>
     )
   }
 }
