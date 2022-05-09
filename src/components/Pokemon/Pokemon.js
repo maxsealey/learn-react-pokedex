5e595a4d162d03b665b5dc6327ef8a2674d2a387
Max Sealey <maxsealey@users.noreply.github.com>
2022-05-09 12:12:56 -0700

    created each Pokemon unit, bootstrapped classes for md and sm window sizes

--- src/components/Pokemon/Pokemon.js
+++ src/components/Pokemon/Pokemon.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './Pokemon.css';
 
-class Pokemon extends React.Component {
+export default class Pokemon extends React.Component {
     render(){
         return (
-            <div>
-
-                
+            <div className = "col-md-3 col-sm-6 mb-5">
+                <div className = "card">
+                    <div className = "card-header">
+                        <h6>Name</h6>
+                    </div>
+                </div>
             </div>
         )
     }
