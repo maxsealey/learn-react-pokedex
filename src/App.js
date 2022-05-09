1c7ca7b5e3cd095eb5c586085eab089ffa2e4d67
Max Sealey <maxsealey@users.noreply.github.com>
2022-05-09 12:15:22 -0700

    render results in App component, styling changes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import NavBar from './components/layout/NavBar';
+import Results from './components/layout/Results';
 
 class App extends React.Component {
   render(){
     return (
       <div className="App">
         <NavBar/>
+        <div className="container">
+          <Results/>
+        </div>
       </div>
     )
   }
