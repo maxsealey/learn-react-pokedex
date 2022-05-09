1cb5076770e62d82cf97e25579877b7c2a8c5a69
Max Sealey <maxsealey@users.noreply.github.com>
2022-05-09 11:49:13 -0700

    imported NavBar component, styling changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import NavBar from './components/layout/NavBar';
 
 class App extends React.Component {
   render(){
     return (
-      <div>
-        
+      <div className="App">
+        <NavBar/>
       </div>
     )
   }
