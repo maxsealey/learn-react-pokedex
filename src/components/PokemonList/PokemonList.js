ccd6cec75c6e3c45ec373617e62edbe001805391
Max Sealey <maxsealey@users.noreply.github.com>
2022-05-10 11:13:23 -0700

    asynchronously retrieves data from poke api, maps each result to Pokemon component

--- src/components/PokemonList/PokemonList.js
+++ src/components/PokemonList/PokemonList.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import Pokemon from '../Pokemon/Pokemon';
-
+import axios from 'axios'
 import './PokemonList.css';
 
 export default class PokemonList extends React.Component {
+
+    state = {
+        url: " https://pokeapi.co/api/v2/pokemon/",
+        pokemon: null // stores retrieved data
+    }
+
+    async componentDidMount() {
+        const res = await axios.get(this.state.url)
+        this.setState({ pokemon: res.data['results'] })
+    } // asynchronously gets data from pokeapi
+
     render(){
         return (
-            <div>
-                <Pokemon/>
-                <Pokemon/>
-                <Pokemon/>
-                <Pokemon/>
-                <Pokemon/>
-                <Pokemon/>
-            </div>
+            <React.Fragment>
+                {
+                    this.state.pokemon ? 
+                    (<div className = "row">
+                        {this.state.pokemon.map(poke => <Pokemon />)}
+                    </div>) : <h1>Loading</h1>
+                }
+            </React.Fragment>
         )
     }
 }
