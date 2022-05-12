429fc4d7b217f75fd3d0323f501c7f03225a91a4
Max Sealey <maxsealey@users.noreply.github.com>
2022-05-12 13:27:25 -0700

    created Sort component

---
+++ src/components/Sort.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css';
+
+class Sort extends React.Component {
+    render(){
+        return (
+            <div>
+
+                
+            </div>
+        )
+    }
+}
