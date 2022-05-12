df2b8291aae2c75596cad8abffa318c974d7d68c
Max Sealey <maxsealey@users.noreply.github.com>
2022-05-12 13:24:59 -0700

    added Fontawesome search, icon, flex and link styling

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 import '../../App.css'
+
 
 export default class NavBar extends Component {
   render() {
     return (
         <div>
             <nav className='navbar navbar-expand-md navbar-dark bg-dark fixed-top' 
-            style={{ height:'50px', display:'flex', justifyContent:'space-around', position:'absolute' }}>
-            <a href="#" style={{ color:'white', textDecoration:'none', margin:'15px 0px 0px 15px',cursor:'pointer' }}>
-            <h5>Pokedex - Gotta Catch 'Em All!</h5></a></nav>
+            style={{ height:'60px', display:'flex', justifyContent:'space-between', position:'absolute' }}>
+            <a href="#" style={{ color:'white', textDecoration:'none',cursor:'pointer' }}>
+            <h5 style={{margin:'auto', marginLeft:'20px'}}>Pokedex - Gotta Catch 'Em All!</h5></a><a href="#">
+            <FontAwesomeIcon icon={faMagnifyingGlass} style={{color:'white', marginRight:'20px', height:'20px',width:'20px'}}/></a></nav>
         </div>
     )
   }
