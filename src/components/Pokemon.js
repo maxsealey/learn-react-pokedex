7b7de2e1b89979cb4b51238382b54c1dc295b22b
Max Sealey <maxsealey@users.noreply.github.com>
2022-05-12 13:25:50 -0700

    Card styling

---
+++ src/components/Pokemon.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import '../App.css';
+
+export default class Pokemon extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            name: '',
+            url: '',
+            pokeIndex: '',
+            imgUrl:''
+        }
+    }
+
+    componentDidMount(){
+        const {name, url} = this.props;
+        const pokeIndex = url.split('/')[url.split('/').length-2]
+        const imgUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokeIndex}.png`
+
+        this.setState({
+            name,
+            url,
+            pokeIndex,
+            imgUrl
+        })
+    }
+
+    render(){
+        return (
+            <div className = "col-md-3 col-sm-6 mb-5">
+                <div className = "card" style={{minHeight:'12em',minWidth:'10em'}}>
+                    <h6 className = "card-header text-center">
+                        Nat. Dex# {this.state.pokeIndex}<br/>  {this.state.name.toUpperCase()}
+                    </h6>
+                    <div className="card-body" style={{display:'flex', justifyContent:'space-around'}}>
+                        <img src={this.state.imgUrl} alt={this.state.name} style={{height:'6em',width:'6em'}}/>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
